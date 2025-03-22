925e0cc27b9f0c3b654149054e14c8e255438225
honey32 <honey32@bearwapps.com>
2025-03-22 12:08:22 +0900

    fix: スタイルの書き方と少し調整

--- apps/web/app/(other)/posts/[slug]/_components/PostHeader.tsx
+++ apps/web/app/(other)/posts/[slug]/_components/PostHeader.tsx
@@ -1,10 +1,8 @@
-import clsx from "clsx";
 import Link from "next/link";
 
 import { Time } from "@/app/_common/date-time-format";
 import { font_poppins } from "@/app/_common/fonts";
-
-import styles from "./PostHeader.module.scss";
+import { styled } from "@repo/style/styled";
 
 type Props = {
   updatedAt: string;
@@ -13,40 +11,85 @@ type Props = {
   tags: string[];
 };
 
+const Breadcrumb = styled("ul")`
+  display: flex;
+  list-style: none;
+`;
+
+const BreadcrumbItem = styled("li")`
+  &::after {
+    content: ">";
+    color: var(--color-text-secondary);
+    margin: 0 0.5rem;
+  }
+`;
+
 export function PostHeader({ updatedAt, createdAt, title, tags }: Props) {
   return (
-    <header>
+    <header
+      css={`
+        display: grid;
+        gap: 0.5rem;
+        padding-inline: var(--margin-h);
+        padding-block-start: var(--margin-h);
+      `}
+    >
       <nav>
-        <ul css={`
-          display: flex;
-          margin: 1rem var(--margin-h) 0;
-
-          & li {
-            list-style: none;
-
-            &::after {
-              content: ">";
-              margin: 0 0.5rem;
-            }
-          }
-        `}>
-          <li>
+        <Breadcrumb        >
+          <BreadcrumbItem>
             <Link href="/">ホーム</Link>
-          </li>
-          <li>
+          </BreadcrumbItem>
+          <BreadcrumbItem>
             <Link href="/posts">記事一覧</Link>
-          </li>
-        </ul>
+          </BreadcrumbItem>
+        </Breadcrumb>
       </nav>
 
-      <h1 className={styles.title}>{title}</h1>
-      <div className={styles.tags}>
-        <span>Tags: </span>
+      <h1
+        css={`
+          text-wrap: pretty;
+          word-break: auto-phrase;
+          font-feature-settings: "palt", "kern";
+          letter-spacing: 0.05rem;
+          font-size: 1.4rem;
+          line-height: 1.2;
+
+          color: var(--color-text-accent);
+
+          @media screen and (min-width: 960px) {
+            font-size: 1.8rem;
+          }
+        `}
+      >
+        {title}
+      </h1>
+      <div
+        css={`
+          color: var(--color-text-secondary);
+          display: flex;
+          gap: 0.5ch;
+
+          & > a {
+            color: inherit;
+          }
+        `}
+      >
+        <span className={font_poppins.className}>Tags: </span>
         {tags.map((tag) => (
           <Link key={tag} href={`/posts?tag=${tag}`}>{`#${tag}`}</Link>
         ))}
       </div>
-      <div className={clsx(styles.dates, font_poppins.className)}>
+      <div
+        className={font_poppins.className}
+        css={`
+          color: var(--color-text-secondary);
+          display: flex;
+          column-gap: 1ch;
+          flex-wrap: wrap;
+          font-size: 0.9rem;
+          line-height: 1;
+        `}
+      >
         <span>
           Updated at:&ensp;
           <Time dateTime={updatedAt} />
