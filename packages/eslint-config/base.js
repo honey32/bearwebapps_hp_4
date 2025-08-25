2f1e869a09f31d9c060197a3a4208bafe6e5f4f3
honey32 <honey32@bearwapps.com>
2025-08-25 17:20:57 +0900

    build: setup import-order automation

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -3,6 +3,7 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
+import importX from "eslint-plugin-import-x";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -13,6 +14,51 @@ export const config = [
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "import-x": importX,
+    },
+    settings: {
+      "import-x/internal-regex": "^@repo/",
+    },
+    rules: {
+      "import-x/order": [
+        "error",
+        {
+          distinctGroup: false,
+          "newlines-between": "always",
+          pathGroupsExcludedImportTypes: [],
+          alphabetize: {
+            order: "desc",
+            caseInsensitive: true,
+          },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+          ],
+          pathGroups: [
+            // フレームワークは builtin 扱い
+            { pattern: "{next,react,react-dom}", group: "builtin" },
+            { pattern: "{next,react,react-dom}/**", group: "builtin" },
+            // subpath-import
+            { pattern: "#*", group: "internal", position: "after" },
+            // ルート相対パス
+            { pattern: "@/**", group: "internal", position: "after" },
+            // css modules は、なるべく最後に置く
+            {
+              pattern: "{.,..}/**/*.module.{css,scss}",
+              group: "unknown",
+              position: "after",
+            },
+          ],
+        },
+      ],
+    },
+  },
   {
     plugins: {
       turbo: turboPlugin,
