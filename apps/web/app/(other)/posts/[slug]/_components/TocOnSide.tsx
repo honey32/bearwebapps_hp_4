b22b3a213567a9a280e787dfdeb71cf2417355b7
honey32 <honey32@bearwapps.com>
2025-10-14 22:30:38 +0900

    refactor: 脱 css module
    sass のコンパイルエラーを無視して新しい property を使いたいため。

--- apps/web/app/(other)/posts/[slug]/_components/TocOnSide.tsx
+++ apps/web/app/(other)/posts/[slug]/_components/TocOnSide.tsx
@@ -3,8 +3,6 @@
 import { createPortal } from "react-dom";
 import { useEffect, useState } from "react";
 
-import styles from "./TocOnSide.module.scss";
-
 type Props = {
   data: HeadingData[];
 };
@@ -40,10 +38,27 @@ function TocOnSide_Impl({ data }: Props) {
   }, []);
 
   return (
-    <nav className={styles.root}>
-      <div className={styles.title}>この記事のコンテンツ</div>
+    <nav
+      css={`
+        padding: 10px 16px 24px 0;
+        background-color: var(--color-bg-card);
+        border-radius: 8px;
+      `}
+    >
+      <div
+        css={`
+          text-align: center;
+        `}
+      >
+        この記事のコンテンツ
+      </div>
 
-      <ul className={styles.ul}>
+      <ul
+        css={`
+          margin-block-start: 16px;
+          list-style: none;
+        `}
+      >
         {data.map((heading) => {
           const id = heading?.data?.id;
           const key = `${heading.value} ${id ?? ""}`;
@@ -51,11 +66,64 @@ function TocOnSide_Impl({ data }: Props) {
           return (
             <li
               key={key}
-              className={styles.item}
               data-depth={heading.depth}
               aria-current={currentItem === id ? "true" : "false"}
+              css={`
+                padding: 4px 0;
+                margin-inline-start: 16px;
+                font-size: 0.9rem;
+                font-feature-settings: "palt", "kern";
+
+                display: grid;
+                grid-template-columns: max-content 1fr;
+                align-items: first baseline;
+
+                text-wrap: pretty;
+                word-break: auto-phrase;
+
+                a:any-link {
+                  color: inherit;
+                }
+
+                color: var(--color-text-secondary);
+
+                &[data-depth="3"] {
+                  & > span:first-child {
+                    margin-inline-end: 24px;
+                    clip-path: circle(4px at 50% 50%);
+                  }
+                }
+
+                &[aria-current="true"] {
+                  color: var(--color-text-accent);
+
+                  & > span:first-child {
+                    opacity: 1;
+                    background-color: var(--color-text-accent);
+                  }
+                }
+
+                &:hover {
+                  color: var(--color-text);
+
+                  a {
+                    text-decoration: none;
+                  }
+                }
+              `}
             >
-              <span className={styles.bullet} />
+              <span
+                css={`
+                  display: inline-block;
+                  width: 12px;
+                  height: 12px;
+                  margin-inline-end: 8px;
+                  clip-path: circle(6px at 50% 50%);
+
+                  background-color: var(--color-text);
+                  opacity: 0.3;
+                `}
+              />
               {id ? (
                 <a href={`#${id}`}>{heading.value}</a>
               ) : (
